0005b68e9f5f0937c7482e8159f35aa86138457d
George Geng <george@Mac.lan>
2026-09-23 12:01:17 -0700

    first commit

---
+++ week2/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  // Create a canvas that fills the entire browser window
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // Set background to black
+  background(0);
+}
